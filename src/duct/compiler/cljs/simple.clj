6c4aed6489b6bf4dd1e9489f25a05b28f89847d8
James Reeves <jreeves@weavejester.com>
2026-03-21 04:25:29 +0000

    Factor out compiler environment
    :duct.compiler.cljs/simple key split into ::compiler-env and ::build.

--- src/duct/compiler/cljs/simple.clj
+++ src/duct/compiler/cljs/simple.clj
@@ -1,7 +1,13 @@
 (ns duct.compiler.cljs.simple
-  (:require [cljs.build.api :as build]
+  (:require [cljs.analyzer.api :as ana]
+            [cljs.build.api :as build]
+            [cljs.closure :as closure]
             [integrant.core :as ig]))
 
-(defmethod ig/init-key :duct.compiler.cljs/simple
-  [_ {:keys [source] :as opts}]
-  (build/build source (dissoc opts :source)))
+(defmethod ig/init-key ::compiler-env [_ opts]
+  (ana/empty-state
+   (-> opts (dissoc :foreign-libs) (closure/add-externs-sources))))
+
+(defmethod ig/init-key ::build
+  [_ {:keys [source compiler-env] :as opts}]
+  (build/build source (dissoc opts :source :compiler-env) compiler-env))
--- test/duct/compiler/cljs/simple_test.clj
+++ test/duct/compiler/cljs/simple_test.clj
@@ -1,16 +1,18 @@
 (ns duct.compiler.cljs.simple-test
   (:require [clojure.java.io :as io]
             [clojure.test :refer [deftest is]]
-            [duct.compiler.cljs.simple]
+            [duct.compiler.cljs.simple :as simple]
             [integrant.core :as ig]))
 
 (deftest build-test
   (try
-    (ig/init-key :duct.compiler.cljs/simple
-                 {:output-dir "target/cljs/js"
-                  :output-to "target/cljs/js/main.js"
-                  :optimizations :none
-                  :main 'duct.compiler.cljs.client-test})
+    (ig/init {::simple/compiler-env {}
+              ::simple/build
+              {:compiler-env (ig/ref ::simple/compiler-env)
+               :output-dir "target/cljs/js"
+               :output-to "target/cljs/js/main.js"
+               :optimizations :none
+               :main 'duct.compiler.cljs.client-test}})
    (is (.exists (io/file "target/cljs/js/main.js")))
    (finally
      (doseq [f (reverse (file-seq (io/file "target/cljs")))]
